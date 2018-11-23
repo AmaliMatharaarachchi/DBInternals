1c8fe0633603cfb12c3a7e6bd5b74423a714a1a2
Amali Lakshika <maamalilakshika@gmail.com>
2018-11-23 18:33:12 +0530

    h2 queries

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Created by ASUS on 2018-11-23.
@@ -28,16 +25,62 @@ public class Main {
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
             //STEP 3: Execute a query
+//            System.out.println("Creating table in given database...");
+//            stmt = conn.createStatement();
+//            String sql = "CREATE TABLE   TEST_H2 " +
+//                    "(id INTEGER not NULL, " +
+//                    " first VARCHAR(255), " +
+//                    " last VARCHAR(255), " +
+//                    " age INTEGER, " +
+//                    " PRIMARY KEY ( id ))";
+//            stmt.executeUpdate(sql);
+//            System.out.println("Created table in given database...");
+
+            /////////////////////////////////////
+
+//            System.out.println("Creating table in given database...");
+//            stmt = conn.createStatement();
+//            String sql = " CREATE INDEX test2 ON TEST_H2(first,last)";
+//            stmt.executeUpdate(sql);
+//            System.out.println("Created table in given database...");
+
             System.out.println("Creating table in given database...");
             stmt = conn.createStatement();
-            String sql = "CREATE TABLE   TEST_H2 " +
-                    "(id INTEGER not NULL, " +
-                    " first VARCHAR(255), " +
-                    " last VARCHAR(255), " +
-                    " age INTEGER, " +
-                    " PRIMARY KEY ( id ))";
-            stmt.executeUpdate(sql);
+            String sql = " SELECT s.INDEXED_COL,GROUP_CONCAT(INDEX_NAME) INDEX_NAMES FROM (" +
+                    "SELECT INDEX_NAME,GROUP_CONCAT(CONCAT(TABLE_NAME,'.',COLUMN_NAME) ORDER BY CONCAT(ORDINAL_POSITION,COLUMN_NAME)) INDEXED_COL FROM INFORMATION_SCHEMA.indexes WHERE table_schema = 'PUBLIC'" +
+                    "AND table_name='TEST_H2' GROUP BY INDEX_NAME" +
+                    ")as s GROUP BY INDEXED_COL HAVING COUNT(1)>1";
+//            stmt.executeQuery(sql);
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                String i = rs.getString("INDEXED_COL");
+                String j = rs.getString("INDEX_NAMES");
+
+                System.out.println(i + "\t"+j
+                       );
+            }
+
             System.out.println("Created table in given database...");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            ///////////////////////////////////////
 
             // STEP 4: Clean-up environment
             stmt.close();
